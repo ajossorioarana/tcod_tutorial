da1e2d3c87ab108f3caba33d2c25946deb591280
Arturo OA <arturoa91@gmail.com>
2022-06-23 23:47:13 -0300

    Up to before Tiles

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
 import tcod
 
-from actions import EscapeAction, MovementAction
+from engine import Engine
+from entity import Entity
 from input_handlers import EventHandler
 
 def main() -> None:
@@ -15,6 +16,13 @@ def main() -> None:
 
     event_handler = EventHandler()
 
+    player = Entity(player_x, player_y, "@", (255, 255, 255))
+    npc = Entity(player_x - 5, player_y, "@", (255, 255, 0))
+
+    entities = {player, npc}
+
+    engine = Engine(entities=entities, event_handler=event_handler, player=player)
+
     with tcod.context.new_terminal(
         screen_width,
         screen_height,
@@ -24,25 +32,12 @@ def main() -> None:
     ) as context:
         root_console = tcod.Console(screen_width, screen_height, order="F")
         while True:
-            root_console.print(x=player_x, y=player_y, string="@")
+            engine.render(console=root_console, context=context)
 
-            context.present(root_console)
+            events = tcod.event.wait()
 
-            root_console.clear()
+            engine.handle_events(events)
 
-            for event in tcod.event.wait():
-                action = event_handler.dispatch(event)
-
-                if action is None:
-                    continue
-
-                if isinstance(action, MovementAction):
-                    player_x += action.dx
-                    player_y += action.dy
-
-                elif isinstance(action, EscapeAction):
-                    raise SystemExit()
-
-
+            
 if __name__ == "__main__":
     main()
